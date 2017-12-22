9f5077f959b20b0c9feddb0fd999bb845e962814
haodonglin <haodonglin@jinlinbao.com>
2017-12-22 14:45:01 +0800

    local cash

--- localcache/filecache.py
+++ localcache/filecache.py
@@ -1,154 +1,171 @@
 #!/usr/bin/env
 # -*- coding: utf-8 -*-
 """
-用本地文件做数据缓存装饰器（被装饰的函数参数不能是不可序列化的对象）
+用本地文件做数据缓存装饰器（被装饰的函数参数以及返回结果必须可序列化的对象）
 """
+import fcntl
 import os
-import json
+import pickle
 import time
-import logging
 import hashlib
 from functools import wraps
-from lib.utils import force_utf8
-
-
-def key_to_int(data):
-    """
-    字典的key转换成int
-    :param data:
-    :return:
-    """
-    if isinstance(data, dict):
-        tmp_dict = {}
-        for k, v in data.iteritems():
-            try:
-                tmp_dict.update({int(k): key_to_int(v)})
-            except ValueError:
-                tmp_dict.update({k: key_to_int(v)})
-        return tmp_dict
-    return data
 
 
 class FileCache(object):
     """
     用本地文件做数据缓存
     """
-    FILE_NAME_LIST = []
+    FILE_NAME_LOCK_LIST = []
 
-    def __init__(self, key, expire=86400, retry_count=50, interval=1.0):
+    def __init__(self, path, expire=86400):
         """
         初始化
         :param int expire: 失效时间, 分
-        :param str key: 指定键值
-        :param int start: 从开始计算的参数下标
-        :param int retry_count: 重试次数
-        :param float interval: 重试间隔, 单位: 秒
+        :param str path: 缓存文件路径
         :return: 装饰器
         """
         self.expire = expire
-        self.retry_count = retry_count
-        self.interval = interval
-        self.key = key
-        filename = key + ".json"
-        self.filename = filename
-        if not os.path.exists("/data/tmp"):
-            os.mkdir("/data/tmp")
+        self.root = "/data/tmp/filecache/"
+        self.path = os.path.join(self.root, path)
+        file_path = os.path.dirname(self.path)
+        # 创建目录
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+        # 创建文件
+        if not os.path.exists(self.path):
+            with open(self.path, "w"):
+                pass
 
     def __call__(self, func):
         """
-        文件缓冲
-        :param args:
-        :param kwargs:
+        文件缓存
+        :param function func: 被装饰的函数
         :return:
         """
 
-        for i in xrange(self.retry_count):
-            if self.filename in self.FILE_NAME_LIST:
-                time.sleep(self.interval)
-                logging.debug("cache file [%s] is using, operate file error", self.filename)
-            else:
-                logging.debug("cache file [%s] get succ", self.filename)
-                can_use = True
-
-        if not can_use:
-            raise RuntimeError("cache file [%s] is using, operate file error" % self.filename)
-
         @wraps(func)
         def wrap_func(*args, **kwargs):
-            self.FILE_NAME_LIST.append(self.filename)
 
-            try:
-                file_name = "/data/tmp/" + self.filename
-                data_str = "{}"
-                if kwargs.get("clear_cache"):
-                    kwargs.pop("clear_cache")
-                else:
-                    # 不清除缓存时，读缓存
-                    if os.path.exists(file_name):
-                        with open(file_name, "r") as f:
-                                data_str = f.read()
-                cache_dict = json.loads(data_str)
-                cache_params_str = cache_dict.get("param_str", "")
-                param_str = self._gen_param_str(args, kwargs)
+            # 获得读锁
+            with open(self.path, "r") as fl:
+                fd = fl.fileno()
+                fcntl.lockf(fd, fcntl.LOCK_SH)
+                cache_dict = self.read_file(fl)
 
-                # 函数参数相同并且在有效期内，缓冲可以使用。否则使用函数，缓冲结果
-                if cache_params_str == param_str and time.time() - int(cache_dict["time_stamp"]) < self.expire:
-                    cache_data = cache_dict["data"]
-                    cache_data = force_utf8(cache_data)
-                    cache_data = key_to_int(cache_data)
-                    result = cache_data
-                else:
-                    result = func(*args, **kwargs)
+            # 获得缓存数据
+            param_hash = self._gen_param_hash(*args, **kwargs)
+            data = self.get_cache_data(cache_dict, param_hash)
+            if data:
+                return data
 
-                # 写缓冲
-                with open(file_name, "w") as f:
-                        cache_dict = {
-                            "key": self.key,
-                            "param_str": param_str,
-                            "time_stamp": time.time(),
-                            "data": result
-                        }
-                        f.write(json.dumps(cache_dict))
-            except Exception as e:
-                self.FILE_NAME_LIST.remove(self.filename)
-                raise e
+            # 获得写锁
+            with open(self.path, "r+") as fl:
+                fd = fl.fileno()
+                fcntl.lockf(fd, fcntl.LOCK_EX)
+                # 获得写锁，查看文件是否已经变更（查看在进程阻塞获得锁的过程中缓存文件是否更新）
+                cache_dict = self.read_file(fl)
 
-            self.FILE_NAME_LIST.remove(self.filename)
-            return result
+                data = self.get_cache_data(cache_dict, param_hash)
+                if data:
+                    return data
+
+                result = func(*args, **kwargs)
+                # 写文件
+                self.write_file(fl, result, param_hash)
+
+                return result
 
         return wrap_func
 
-    def _gen_param_str(self, args, kwargs):
+    def _gen_param_hash(self, *args, **kwargs):
         """
         生成函数参数指纹
+        :param list args: 位置参数列表
         :param list args: 位置参数列表
         :param dict kwargs: 关键字参数
         :return: str 函数参数指纹
         """
         args = args[1:]
         code = hashlib.md5()
-        code.update(str(self.key))
+        for i in args:
+            if isinstance(i, list):
+                i.sort()
         code.update("".join(sorted([str(i) for i in args])))
-        code.update("".join(sorted([str(i) for i in kwargs.iteritems()])))
-        return code.hexdigest()
+        code.update("".join(sorted([str(i) for i in kwargs.items()])))
+        res = code.hexdigest()
+        return res
+
+    def read_file(self, fl):
+        """
+        读文件
+        :param File fl:文件
+        :return:
+        """
+        try:
+            cache_dict = pickle.load(fl)
+        except Exception:
+            cache_dict = {}
+        return cache_dict if cache_dict else {}
+
+    def write_file(self, fl, data, param_hash):
+        """
+        写文件
+        :param File fl: 文件
+        :param data: 数据
+        :param str params_hash: 参数指纹
+        :return:
+        """
+        cache_dict = {
+            "param_str": param_hash,
+            "time_stamp": time.time(),
+            "data": data
+        }
+        fl.seek(0, os.SEEK_SET)
+        fl.truncate()
+        pickle.dump(cache_dict, fl)
+        fl.flush()  # 强制写回到文件
+
+    def get_cache_data(self, cache_dict, params_hash):
+        """
+        获得数据
+        :param dict cache_dict: 获得缓存数据
+        :param str params_hash: 参数指纹
+        :return:
+        """
+        cache_params_str = cache_dict.get("param_str", "")
+
+        # 判断缓存是否可用
+        if cache_params_str == params_hash and time.time() - cache_dict["time_stamp"] < self.expire:
+            cache_data = cache_dict["data"]
+            return cache_data
+        else:
+            return None
 
     def clear_cache(self):
         """
         清除cache
         :return: None
         """
-        file_name = "/data/tmp/" + self.filename
-        self.FILE_NAME_LIST.append(self.filename)
+        with open(self.path, "w") as file:
+            fd = file.fileno
+            fcntl.fcntl(fd, fcntl.LOCK_EX)
+            file.write("")
 
-        with open(file_name, "w") as file:
-            file.write("{}")
-
-        self.FILE_NAME_LIST.remove(self.filename)
 
 if __name__ == "__main__":
-    @FileCache("add")
-    def add(a, b):
-        print "no cache"
-        return a + b
+    class Test(object):
+        @FileCache("tmp/add.txt")
+        def add(self, a, b):
+            print ("no cache")
+            time.sleep(1)
+            return a + b
 
-    c = add(1, 2)
+
+    from multiprocessing import Process
+
+    for i in range(5):
+        print (" params %s" % i)
+        for _ in range(10):
+            p = Process(target=Test().add, args=(1, i))
+            p.start()
+            p.join()
