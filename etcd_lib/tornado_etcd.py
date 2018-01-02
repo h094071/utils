1889e12b477137253e60873983f7137d44ba4995
haodonglin <haodonglin@jinlinbao.com>
2018-01-02 21:00:07 +0800

    etcd: add doc

--- etcd_lib/tornado_etcd.py
+++ etcd_lib/tornado_etcd.py
@@ -20,14 +20,15 @@ from singleton import Singleton
 
 class EtcdProcess(object):
     """"
-    etcd 处理基类
+    etcd 线程
     """
 
     def __init__(self, watch_root, etcd_servers, sentry=None):
         """
         init
-        :param str key: 监控的关键字
+        :param str watch_root: 监控的根目录
         :param tuple etcd_servers: etcd 服务器列表
+        :param dict sentry: sentry object
         """
         self.watch_root = watch_root
         self.client = etcd.Client(host=etcd_servers, allow_reconnect=True)
@@ -56,7 +57,7 @@ class EtcdProcess(object):
                     self.key_process_dict[process_dir](response)
 
             except Exception as exp:
-                logging.error("thread error: %s", str(exp))
+                logging.error("tornado etcd thread error: %s", str(exp))
                 if self.sentry:
                     self.sentry.captureException(exc_info=True)
 
@@ -70,23 +71,23 @@ class EtcdProcess(object):
             self.thread = t
             t.setDaemon(True)
             t.start()
-            logging.error("start thread")
+            logging.info("tornado etcd start thread")
 
     def register(self, key, process):
         """
-
-        :param key:
-        :param process:
+        注册处理器 eg: key="lock" process=lock_process
+        :param str key: 关注的目录
+        :param func process: 处理器
         :return:
         """
         if key in self.key_process_dict:
-            raise RuntimeError("key: %s has existed" % key)
+            raise RuntimeError("tornado etcd key: %s has existed" % key)
         self.key_process_dict[key] = process
 
 
 class ProcessBase(Singleton):
     """
-
+    处理器基类
     """
 
     def __init__(self, watch_root, etcd_servers, sentry=None):
@@ -104,9 +105,9 @@ class ProcessBase(Singleton):
 
     def get_key(self, key):
         """
-
-        :param key:
-        :return:
+        获得带路径的key值 eg: key = "read_lock" return /base_dir/lock/read_lock
+        :param str key: 部分key值
+        :return: "获得带路径的key值"
         """
         dir_list = self.watch_root.split("/")
         dir_list.extend([self.base_key, key])
@@ -114,27 +115,43 @@ class ProcessBase(Singleton):
 
     def test_and_set(self, key, value, prev_value, ttl=None):
         """
-
-        :param key:
-        :param value:
-        :param prev_value:
-        :param ttl:
+        etcd test_and_set 封装
+        :param str key: 部分key值
+        :param value: 设置的值
+        :param prev_value: 设置前的值
+        :param ttl: 过期时间
         :return:
         """
         key = self.get_key(key)
         return self.client.test_and_set(key, value, prev_value, ttl)
 
     def set(self, key, value, ttl=None, dir=False, append=False, **kwdargs):
+        """
+        etcd set 封装
+        :param key: 部分key值
+        :param value: 设置的值
+        :param ttl: 过期时间
+        :param dir: 目录
+        :param append: 
+        :param kwdargs: 
+        :return: 
+        """
         key = self.get_key(key)
         return self.client.write(key, value, ttl, dir, append, **kwdargs)
 
     def get(self, key, **kwdargs):
+        """
+        etcd get 封装
+        :param key: 
+        :param kwdargs: 
+        :return: 
+        """
         key = self.get_key(key)
         return self.client.read(key, **kwdargs)
 
     def process(self, etcd_value):
         """
-
+        处理器
         :param etcd_value:
         :return:
         """
@@ -143,7 +160,7 @@ class ProcessBase(Singleton):
 
 class WatchProcess(ProcessBase):
     """
-    监控关键字，
+    监控关键字，关键字不变化，协程阻塞，关键字有变化，协程继续执行
     """
 
     def __init__(self, watch_root, etcd_servers, sentry=None):
@@ -158,8 +175,8 @@ class WatchProcess(ProcessBase):
 
     def watch_key(self, key):
         """
-
-        :param key:
+        监控关键字，关键字不变化，协程阻塞，关键字有变化，协程继续执行
+        :param str key: 监控的关键字
         :return:
         """
         self.etcd_process._start_thread()
@@ -170,7 +187,7 @@ class WatchProcess(ProcessBase):
 
     def process(self, etcd_res):
         """
-
+        监控关键字处理器
         :param etcd_res:
         :return:
         """
@@ -207,7 +224,7 @@ class LockProcess(ProcessBase):
 
     def process(self, etcd_res):
         """
-
+        锁处理器
         :param etcd_res:
         :return:
         """
@@ -219,9 +236,10 @@ class LockProcess(ProcessBase):
         # 解锁状态 或者 锁超时 进行解锁操作
         if etcd_value in (self.UNLOCK, None) and wait_lock_list:
             lock_item = wait_lock_list[0]
-            logging.error("watch lock")
             lock_flag = self._lock(etcd_key, lock_item)
+
             if lock_flag:
+                logging.info("tornado etcd 加锁成功 key:%s", etcd_key)
                 lock_item.future.set_result((True, lock_item.token))
                 self.locking_dict[etcd_key] = lock_item
                 self.wait_lock_dict[etcd_key].pop(0)
@@ -235,10 +253,10 @@ class LockProcess(ProcessBase):
 
     def _lock(self, key, lock_item):
         """
-
+        尝试加锁
         :param str key: 关键字
         :param str lock_uuid: lock码
-        :return: bool
+        :return: bool 加锁是否成功
         """
         token = lock_item.token
         ttl = lock_item.ttl
@@ -286,11 +304,10 @@ class LockProcess(ProcessBase):
         """
         try:
             self.test_and_set(key, self.UNLOCK, token)
-            logging.error("解锁 %s", token)
+            logging.info("tornado etcd 解锁 key: %s, token: %s", key, token)
             return True
         except (etcd.EtcdCompareFailed, etcd.EtcdKeyNotFound) as e:
-            logging.error("在未加锁的情况下，进行解锁操作")
-            logging.error(e)
+            logging.error("在未加锁的情况下，进行解锁操作 %s", e)
             return False
 
 
@@ -320,6 +337,7 @@ if __name__ == "__main__":
 
         io_loop.start()
 
+
     @gen.coroutine
     def test():
         # res = yield etcd_watch.get("/watch/a")
@@ -345,6 +363,7 @@ if __name__ == "__main__":
         logging.error("执行时间：")
         logging.error(time.time() - start_time)
         raise gen.Return(res)
+
 
     start_loop([test, test, test, test, test, test, test, test, test, test, test, test, ])
     # IOLoop.current().run_sync(test)
